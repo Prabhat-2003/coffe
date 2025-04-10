1c9b3fdc32cf90f063ac47b56ce6cdd631d53965
prabhat <basugupta2003@gmail.com>
2025-04-10 17:29:49 +0530

    upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// // 
+// const menuOpenButton = document.querySelector("#menu-open-button");
+// const menuCloseButton = document.querySelector("#menu-close-button");
+// const body = document.body;
+
+// // Open menu
+// menuOpenButton.addEventListener("click", () => {
+//     document.body.classList.toggle("show-mobile-menu");
+// });
+
+// // // Close menu
+// // menuCloseButton.addEventListener("click", () => {
+// //     body.classList.remove("show-mobile-menu");
+// // });
+// // document.addEventListener("DOMContentLoaded", function () {
+// //     const menuOpenButton = document.querySelector("#menu-open-button");
+// //     const menuCloseButton = document.querySelector("#menu-close-button");
+// //     const body = document.body;
+
+// //     if (menuOpenButton && menuCloseButton) {
+// //         // Open menu
+// //         menuOpenButton.addEventListener("click", () => {
+// //             body.classList.add("show-mobile-menu");
+// //         });
+
+// //         // Close menu
+// //         menuCloseButton.addEventListener("click", () => {
+// //             body.classList.remove("show-mobile-menu");
+// //         });
+// //     }
+// // });
+document.addEventListener("DOMContentLoaded", function () {
+    const menuOpenButton = document.querySelector("#menu-open-button");
+    const menuCloseButton = document.querySelector("#menu-close-button");
+    const body = document.body;
+
+    if (menuOpenButton && menuCloseButton) {
+      menuOpenButton.addEventListener("click", () => {
+        body.classList.add("show-mobile-menu");
+      });
+
+      menuCloseButton.addEventListener("click", () => {
+        body.classList.remove("show-mobile-menu");
+      });
+    }
+  });
