cf258dd1540b8a89c10326ff9a697989564ec81e
prabhat <basugupta2003@gmail.com>
2025-04-14 17:18:41 +0530

    html css complete

--- script.js
+++ script.js
@@ -1,90 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuOpenButton = document.querySelector("#menu-open-button");
-    const menuCloseButton = document.querySelector("#menu-close-button");
-    const body = document.body;
+  const menuOpenButton = document.querySelector("#menu-open-button");
+  const menuCloseButton = document.querySelector("#menu-close-button");
+  const body = document.body;
 
-    if (menuOpenButton && menuCloseButton) {
-      menuOpenButton.addEventListener("click", () => {
-        body.classList.add("show-mobile-menu");
+  if (menuOpenButton && menuCloseButton) {
+    menuOpenButton.addEventListener("click", () => {
+      body.classList.add("show-mobile-menu");
+    });
+
+    menuCloseButton.addEventListener("click", () => {
+      body.classList.remove("show-mobile-menu");
+    });
+  }
+});
+window.addEventListener("load", function () {
+  const video = document.getElementById("bg-video");
+
+  if (video) {
+    video.muted = true;
+    video.playsInline = true;
+    video.loop = true;
+    video.setAttribute("playsinline", "");
+    video.setAttribute("muted", "");
+    video.setAttribute("autoplay", "");
+    video.load();
+
+    video
+      .play()
+      .then(() => {
+        console.log("Video playing");
+      })
+      .catch((err) => {
+        console.warn(
+          "Autoplay failed, maybe user interaction is required",
+          err
+        );
       });
+  }
+});
 
-      menuCloseButton.addEventListener("click", () => {
-        body.classList.remove("show-mobile-menu");
-      });
-    }
-  });
-  // window.addEventListener("load", function () {
-  //   const video = document.getElementById("bg-video");
+const swiper = new Swiper(".slide-wrapper", {
+  loop: true,
+  spaceBetween: 25,
+  grabCursor: 25,
 
-  //   if (video) {
-  //     video.muted = true;
-  //     video.playsInline = true;
-  //     video.setAttribute('playsinline', '');
-  //     video.setAttribute('muted', '');
-  //     video.setAttribute('autoplay', '');
-  //     video.load();
-  //   }
-  //   // try to force play
-  //   video
-  //     .play()
-  //     .then(() => {
-  //       console.log("Video playing");
-  //     })
-  //     .catch((err) => {
-  //       console.warn("Autoplay failed, maybe user interaction is required", err);
-  //     });
-  // });
-  window.addEventListener("load", function () {
-    const video = document.getElementById("bg-video");
-  
-    if (video) {
-      video.muted = true;
-      video.playsInline = true;
-      video.loop = true;
-      video.setAttribute('playsinline', '');
-      video.setAttribute('muted', '');
-      video.setAttribute('autoplay', '');
-      video.load();
-  
-      video
-        .play()
-        .then(() => {
-          console.log("Video playing");
-        })
-        .catch((err) => {
-          console.warn("Autoplay failed, maybe user interaction is required", err);
-        });
-    }
-  });
-  
-  const swiper = new Swiper('.slide-wrapper', {
-    loop: true,
-    spaceBetween: 25,
-    grabCursor:25,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    //responsive breakpoints
-    breakpoints: {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //responsive breakpoints
+  breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3
-    }
-  }
- });
-  
+      slidesPerView: 3,
+    },
+  },
+});
